fad7915ba453157a57c6f582602a50a1aa2788ff
CamNC2003 <camnc2003@gmail.com>
2024-09-29 22:47:19 -0600

    Week 5 Assignment

---
+++ class-app1/src/app/shared/pipes/size.pipe.ts
@@ -0,0 +1,11 @@
+// size.pipe.ts
+import {Pipe, PipeTransform} from "@angular/core";
+
+@Pipe({
+  name: 'size',
+})
+export class SizePipe implements PipeTransform {
+  transform(size: number): string {
+    return `(${size}")`;
+  }
+}
