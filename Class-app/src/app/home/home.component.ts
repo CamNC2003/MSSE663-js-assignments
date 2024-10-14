43580b4d9df27f524bf3178634882c61d07b33e6
CamNC2003 <camnc2003@gmail.com>
2024-10-13 21:13:45 -0600

    Week 7 Assignment

--- Class-app/src/app/home/home.component.ts
+++ Class-app/src/app/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Store } from '@ngrx/store';
+import { Store } from "@ngrx/store";
 import { PizzasState, selectPizzas } from '../pizza-app/state';
 
 @Component({
